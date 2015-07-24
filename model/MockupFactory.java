41554a1c9eec98d489ef6965afbd7800cffb491c
JonathanMurray <jonte_murray@hotmail.com>
2015-07-24 23:46:45 +0200

    Random mockupfactory

--- model/MockupFactory.java
+++ model/MockupFactory.java
@@ -19,7 +19,7 @@ public class MockupFactory {
         GridModel grid = new GridModel(numCols, numRows);
         for(int x = 0; x < numCols; x++){
             for(int y =0; y < numRows; y++){
-                grid.setCharAtCell('X', new Cell(x,y));
+                grid.setCharAtCell(Util.randomLetter(), new Cell(x,y));
             }
         }
         return grid;
